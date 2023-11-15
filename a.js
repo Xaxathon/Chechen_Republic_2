383eb2bed4eb1c6672c5d8bae96475cac6ef0bb2
Xaxathon <149861091+Xaxathon@users.noreply.github.com>
2023-11-15 18:22:07 +0300

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,10 @@
 // Мой репизиторий
+let a = [3,4,5,6,34,5,34,3,54];
+for (let i = 0; i < a.length; i++) {
+  let b = a[i];
+  if (b > 5) {
+    console.log("Крутой")
+  } else {
+    console.log("Не крутой")
+  }
+}
